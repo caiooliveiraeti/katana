498eee24674ace4dc43574e38b05f297c74b52ae
Caio Oliveira <caio@caiooliveira.eti.br>
2015-08-29 19:32:33 -0300

    Finalizando fluxo de autenticacao.

--- thack/homepage/urls.py
+++ thack/homepage/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 import views
 
 urlpatterns = [
-    url(r'^$', views.Home.as_view()),
+    url(r'^$', login_required(views.Home.as_view())),
     url(r'^logout/$', 'homepage.views.logout_view', name='logout'),
     url(r'^accounts/profile/$', views.ProfileView.as_view(), name='profile'),
     url(r'^home', login_required(views.Home.as_view())),
