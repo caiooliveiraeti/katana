b6f5991fd81030c9e20a0ff1f0a90fc72b9bc2ab
Paulo Scardine <paulos@xtend.com.br>
2015-08-29 23:56:15 -0300

    api sample

--- thack/api/views.py
+++ thack/api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
 from rest_framework.views import APIView
+from django.conf import settings
 
 
 class ApiSample(View):
