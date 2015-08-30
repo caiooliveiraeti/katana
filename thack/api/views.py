c7cf01f4e386fc25c550944e2dc9e51756990991
Caio Oliveira <caio@caiooliveira.eti.br>
2015-08-30 00:24:03 -0300

    Adicionando rest api

--- thack/api/views.py
+++ thack/api/views.py
@@ -9,3 +9,20 @@ from django.conf import settings
 class ApiSample(View):
     def get(self, request):
         return HttpResponse(json.dumps(request.GET), content_type='application/json')
+
+class EventsApi(View):
+    def get(self, request):
+        return HttpResponse(json.dumps(request.GET), content_type='application/json')
+
+
+class EventFares(View):
+    def get(self, request):
+    	sabre = Sabre(settings.SABRE_KEY, settings.SABRE_SECRET)
+		fares = sabre.api.v1.historical.flights.fares(
+			origin=request.GET['origin'],
+			destination=request.GET['origin'],
+			earliestdeparturedate=request.GET['earliestdeparturedate'],
+			latestdeparturedate=request.GET['latestdeparturedate'],
+			lengthofstay=7,
+		)
+        return HttpResponse(json.dumps(fares), content_type='application/json')
