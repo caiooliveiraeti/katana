c2aebe832f14b7a18815be34e8f15626c622396a
Caio Oliveira <caio@caiooliveira.eti.br>
2015-08-30 04:28:05 -0300

    inicio exibicao do detalhe.

--- thack/homepage/urls.py
+++ thack/homepage/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', login_required(views.HomeView.as_view())),
     url(r'^logout/$', 'homepage.views.logout_view', name='logout'),
     url(r'^home', login_required(views.HomeView.as_view())),
+    url(r'^eventDetail', login_required(views.EventDetailView.as_view())),
     url(r'^profile', views.ProfileView.as_view(), name='profile'),
     url(r'^index', views.IndexView.as_view()),
 ]
--- thack/homepage/views.py
+++ thack/homepage/views.py
@@ -15,6 +15,15 @@ class HomeView(TemplateView):
 class ProfileView(TemplateView):
     template_name = 'profile.html'
 
+class EventDetailView(TemplateView):
+
+    template_name = 'event_detail.html'
+
+    def get_context_data(self, **kwargs):
+        d = super(EventDetailView, self).get_context_data(**kwargs)
+        d['event_id'] = self.request.GET['event_id']
+        return d
+
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
